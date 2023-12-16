c93a6d07358886570a30dc070babd49c8364c4ba
NoOneForever <152205739+NepTuneForever@users.noreply.github.com>
2023-12-15 21:32:16 -0300

    Calculator.py
    The Calculator System.

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+x = int(input("Digite um numero:"))
+y = int(input("Digite outro numero:"))
+
+s = x + y
+sub = x - y
+mt = x * y
+div = x / y
+
+print("{} + {} = {}".format(x, y, s))
+print("{} - {} = {}".format(x, y, sub))
+print("{} x {} = {}".format(x, y, mt))
+print("{} ÷ {} = {}".format(x, y, div))
